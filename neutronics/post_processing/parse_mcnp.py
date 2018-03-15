34efeeeae9b45973173dae4d207bc1f02fa2367b
Alexander Swenson <aaswenson@wisc.edu>
2018-03-15 10:46:43 -0500

    added slicing plots, fixed up contour plot with labels, titles

--- neutronics/post_processing/parse_mcnp.py
+++ neutronics/post_processing/parse_mcnp.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import matplotlib.axis
 from matplotlib import cm, rc
 from matplotlib.ticker import LinearLocator, FormatStrFormatter, ScalarFormatter
+import os
 import glob
 import zipfile
 import numpy as np
@@ -43,9 +44,9 @@ def parse_output(filename, n_steps):
 def unzip_collect_outputs():
     """Extract output files and collect their info
     """
-
-    zip_ref = zipfile.ZipFile('depl_results.zip', 'r') 
-    zip_ref.extractall("results")
+    if os.path.isdir('./results') == False:
+        zip_ref = zipfile.ZipFile('depl_results.zip', 'r') 
+        zip_ref.extractall("results")
     files = glob.glob("./results/*")
     
     return files
@@ -63,8 +64,8 @@ def EOL_keff(outputs):
     """For file in outputs
     """
     N = len(outputs)
-    data = np.zeros(N, dtype={'names' : ['r', 'frac', 'EOL_k'],
-                              'formats' : ['f8', 'f8', 'f8']})
+    data = np.zeros(N, dtype={'names' : ['r', 'frac', 'EOL_k', 'grad'],
+                              'formats' : ['f8', 'f8', 'f8', 'f8']})
     results = []
     for idx, file in enumerate(outputs):
         frac, r = process_filename(file)
@@ -72,27 +73,74 @@ def EOL_keff(outputs):
         data[idx]['r'] = r
         data[idx]['frac'] = frac
         data[idx]['EOL_k'] = k[-1]
-    
-    grad = np.gradient(data['EOL_k'])
+    data['grad'] = np.gradient(data['EOL_k'])
+        
+    return data
+
+def plot_grad_keff(data):
+
     Nx = len(set(data['r']))
     Ny = len(set(data['frac']))
     
     # setup the figure
-    fig, (ax1, ax2) = plt.subplots(1,2, sharex=True, sharey=True)
+    fig, ax2 = plt.subplots(1,1, sharex=True, sharey=True)
     # plot keff
-    cs1 = ax1.tricontourf(data['r'],data['frac'], grad, 20)
-    ax1.plot(data['r'],data['frac'], 'ko ')
+#    cs1 = ax1.tricontourf(data['r'],data['frac'], grad, 20)
+#    ax1.plot(data['r'],data['frac'], 'ko ')
     # plot grad keff
     cs2 = ax2.tricontourf(data['r'], data['frac'], data['EOL_k'], 20)
     ax2.plot(data['r'],data['frac'], 'ko ')
-    ax1.set_title("Gradient Map")
+#    ax1.set_title("Gradient Map")
     # Add a color bar which maps values to colors.
-    fig.colorbar(cs1, ax=ax1)
+#    fig.colorbar(cs1, ax=ax1)
     fig.colorbar(cs2, ax=ax2)
-    ax1.set_title("Gradient Map")
+#    ax1.set_title("Gradient Map")
     ax2.set_title("EOL Keff")
     plt.show()
 
+def plot_contour(data):
+    
+    fig, ax = plt.subplots(1,1)
+    cs = ax.tricontourf(data['r'], data['frac'], data['EOL_k'], 10)
+    ax.scatter(data['r'],data['frac'], s=12, facecolors='none', edgecolors='k')
+    fig.colorbar(cs)
+    plt.xlabel("Core Radius [cm]")
+    plt.ylabel("Fuel Fraction [cm]")
+    plt.title("EOL Reactivity - 10y @ 99 kW")
+    plt.show()
+
+def plot_slice(slice_choice, data):
+    
+    slice_datakey = slice_choice[0]
+    slice_val = slice_choice[1]
+    ind_datakey = slice_choice[2]
+    
+    X = []
+    Y = []
+
+    for idx, item in enumerate(data[slice_datakey]):
+        if item == slice_val:
+            X.append(data[ind_datakey][idx])
+            Y.append(data['EOL_k'][idx])
+    
+    
+    fig = plt.figure()
+    plt.scatter(X, Y)
+    xlabels = {'r' : 'Fuel Fraction [-]',
+               'frac' : 'Core Radius [cm]'
+              }
+    titles = {'r':'EOL Reactivity vs. Fuel Fraction at Core Radius: {0} [cm]',
+              'frac': 'EOL Reactivity vs. Core Radius at Fuel Frac: {0}'
+             }
+    title_str = titles[slice_datakey].format(slice_val)
+    plt.title(title_str)
+    plt.xlabel(xlabels[slice_datakey])
+    plt.ylabel('EOL Reactivity [-]')
+    plt.show()
+
+
 testfiles = unzip_collect_outputs()
-EOL_keff(testfiles)
+data = EOL_keff(testfiles)
+
+plot_slice(('frac', 0.5, 'r'), data)
 
